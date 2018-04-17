4d5c190205751912c8ae171c5d8c10cb547c4263
User <User@ASELAS>
2018-04-17 14:29:07 +0530

    new branch changes

--- DemoGit/src/com/scit/demo/HelloWorld.java
+++ DemoGit/src/com/scit/demo/HelloWorld.java
@@ -2,13 +2,8 @@ package com.scit.demo;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// Call to show method
-		show();
-	}
-
-	public static void show() {
-			System.out.println("Execute  show");
-			System.out.println("Test 1");
+	public void show() {
+		System.out.println("Execute  show");
+		System.out.println("Test 1");
 	}
 }
